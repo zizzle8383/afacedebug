2621974380e532d898a82062cddef2f3c422d4b8
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2021-06-30 10:30:21 +1200

    Update setscore.js

--- setscore.js
+++ setscore.js
@@ -3,7 +3,9 @@ function load(){
   console.log("executing..")
   if (x == undefined || x == null){
     document.getElementById("Highbox").innerHTML = "0"
+    console.log("Set!")
   }else{
+    console.log("Set!")
     x = x.toString();
     document.getElementById("Highbox").innerHTML = x
   }
