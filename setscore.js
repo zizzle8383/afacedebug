e9a53ca881630a3a4942f5980defbc1d1e734a77
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2021-06-30 10:28:53 +1200

    Update setscore.js

--- setscore.js
+++ setscore.js
@@ -1,8 +1,10 @@
 function load(){
   x = localStorage.getItem("score")
-  if (x == undefined){
+  console.log("executing..")
+  if (x == undefined || x == null){
     document.getElementById("Highbox").innerHTML = "0"
   }else{
+    x = x.toString();
     document.getElementById("Highbox").innerHTML = x
   }
 }
